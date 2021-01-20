b4187bb692d74cea6ad5b24f4cc4a0805d38866a
Frederic Olivier <jpolivierr@gmail.com>
2021-01-19 23:47:50 -0500

    completed styling for the pledge section and mission section

--- client/src/component/Our-mission/OurMission.js
+++ client/src/component/Our-mission/OurMission.js
@@ -5,20 +5,21 @@ function OurMission() {
   return (
     <div className="section-style our-mission">
       <div className="content-box-container">
-        <div className="mission-box-img">
-
-        </div>
+        <div className="mission-box-img"></div>
         <div className="content-box our-mission-content-box">
-          <h2>Our Mission</h2>
+          <h2>Our <br/> Mission</h2>
           <div className="sub-title">Support our kids in haiti.</div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
             alias eveniet nihil accusamus corrupti! Est ipsum esse, placeat et
-            sequi exercitationem rem ratione minima, necessitatibus eligendi
-            incidunt facilis modi recusandae.
+            sequi exercitationem rem ratione minima,
           </p>
-          {/* <div className="main-button">SUPPORT US</div> */}
         </div>
+      </div>
+      <div className="circles circles-for-mision">
+        <div className="inner-circle-1"></div>
+        <div className="inner-circle-2"></div>
+        <div className="inner-circle-3"></div>
       </div>
     </div>
   )
--- client/src/component/The-pledge/ThePledge.js
+++ client/src/component/The-pledge/ThePledge.js
@@ -1,22 +1,31 @@
-import './ThePledge.css'
+import "./ThePledge.css"
 
-function ThePledge (props){
-    return(
-        <div className="section-style the-pledge">
-        <div className="content-box-container">
-          <div className="content-box the-pledge-content-box">
-            <h2>Take <br></br> The Pledge</h2>
-            {/* <div className="sub-title">Support our kids in haiti.</div> */}
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
-              alias eveniet nihil accusamus corrupti! Est ipsum esse, placeat et
-              sequi exercitationem rem ratione minima, necessitatibus eligendi
-              incidunt facilis modi recusandae.
-            </p>
-          </div>
+function ThePledge(props) {
+  return (
+    <div className="section-style the-pledge">
+      <div className="the-pledge-background"></div>
+      <div className="content-box-container with-pledge ">
+        
+        <div className="content-box the-pledge-content-box">
+          <h2>
+            Take <br></br> The Pledge
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            alias eveniet nihil accusamus corrupti! Est ipsum esse, placeat et
+            sequi exercitationem rem ratione minima, necessitatibus eligendi
+            incidunt facilis modi recusandae.
+          </p>
         </div>
+        <div className="mission-box-img with-pledge"></div>
       </div>
-    )
+      <div className="circles circles-for-pledge">
+        <div className="inner-circle-1"></div>
+        <div className="inner-circle-2"></div>
+        <div className="inner-circle-3"></div>
+      </div>
+    </div>
+  )
 }
 
-export default ThePledge 
+export default ThePledge
